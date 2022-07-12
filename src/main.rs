36465c0cfe1beef2c7c583c68a518c7b724338c6
Kiran Shila <me@kiranshila.com>
2022-07-12 13:46:51 -0700

    First commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::net::UdpSocket;
+use std::time::Instant;
+
+fn main() -> std::io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:6000")?;
+    let mut buf = [0u8; 8192];
+    let mut cnt = 0u64;
+    let mut last_reported = Instant::now();
+    loop {
+        socket.recv(&mut buf)?;
+        cnt += 1;
+        if last_reported.elapsed().as_secs_f32() >= 1.0 {
+            last_reported = Instant::now();
+            println!("{cnt} - Payloads");
+        }
+    }
+}
