08e3ea9af8b5cd4164a5d43bc5b7eeabb8066991
Kiran Shila <me@kiranshila.com>
2022-07-12 14:11:52 -0700

    Measure rate

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,19 @@ use std::time::Instant;
 fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind("192.168.5.1:60000")?;
     let mut buf = [0u8; 8192];
-    let mut work_time = 0f64;
-    let mut idle_time = 0f64;
+    let mut cnt = 0u64;
 
     let mut last_reported = Instant::now();
-    let mut b = Instant::now();
+    let program_start = Instant::now();
     loop {
-        let a = Instant::now();
-        work_time += a.duration_since(b).as_secs_f64();
         socket.recv(&mut buf)?;
-        b = Instant::now();
-        idle_time += b.duration_since(a).as_secs_f64();
+        cnt += 8192;
         if last_reported.elapsed().as_secs_f32() >= 1.0 {
             last_reported = Instant::now();
-            println!("Work Ratio - {}", work_time / (work_time + idle_time));
+            println!(
+                "Rate - {}",
+                (cnt as f64) / program_start.elapsed().as_secs_f64()
+            );
         }
     }
 }
