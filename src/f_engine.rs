a195983d6264acb175ac42610a8c4e883995a38b
Kiran Shila <me@kiranshila.com>
2022-07-12 16:14:59 -0700

    F engine

---
+++ src/f_engine.rs
@@ -0,0 +1,40 @@
+use rustfft::{algorithm::Radix4, num_complex::Complex, num_traits::FromPrimitive, Fft};
+// For each RXed payload from UDP, we will have 16384 samples at 2ns
+
+// To create a stream of channels, we need to
+// * multiply with window function
+// * Fold by (N) taps
+// * FFT
+//
+// As the output is 2048 and our input in 16384, we'll choose 8 taps
+
+pub(crate) fn channelize<T, const N: usize>(input: &[u8], fft: &Radix4<T>) -> [Complex<T>; N]
+where
+    T: rustfft::FftNum
+        + std::default::Default
+        + std::ops::AddAssign
+        + std::ops::SubAssign
+        + std::ops::MulAssign
+        + std::ops::DivAssign
+        + std::ops::RemAssign,
+{
+    assert_eq!(
+        input.len() % N,
+        0,
+        "Output size must divde evenly input size"
+    );
+    let taps = input.len() / N;
+    // Multiply time series by window function
+    // TODO!
+    // Fold
+    let mut folded = [Default::default(); N];
+    for chunk in input.chunks_exact(taps) {
+        for i in 0..chunk.len() {
+            folded[i] += Complex::<T>::from_u8(chunk[i]).unwrap();
+        }
+    }
+    // FFT
+    fft.process(&mut folded);
+    // Return
+    folded
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+mod f_engine;
+
+use f_engine::channelize;
+use rustfft::{algorithm::Radix4, num_complex::Complex, FftDirection};
 use std::net::UdpSocket;
 use std::time::Instant;
+use std::{io, process};
 
 const PAYLOAD_SIZE: usize = 8192;
 const WORD_SIZE: usize = 8;
@@ -12,12 +17,19 @@ fn main() -> std::io::Result<()> {
     let mut last_reported = Instant::now();
     let program_start = Instant::now();
 
+    // Sample time is 2ns (AHHHHHHH)
     let mut pol_a_time_series = [0u8; PAYLOAD_SIZE * 2];
     let mut pol_b_time_series = [0u8; PAYLOAD_SIZE * 2];
+
+    let mut spectra = [0f32; 2048];
+
+    // Setup FFT
+    let fft = Radix4::new(4096, FftDirection::Forward);
 
     loop {
         // Grab incoming data
         socket.recv(&mut buf)?;
+
         // Extract time series
         for (i, word) in buf.chunks_exact(WORD_SIZE).enumerate() {
             // Top of word is all zeros (for now)
@@ -28,13 +40,28 @@ fn main() -> std::io::Result<()> {
             pol_a_time_series[2 * i + 1] = word[3];
         }
 
+        // Channelize
+        let channelized_a: [Complex<f32>; 2048] = channelize(&pol_a_time_series, &fft);
+        let channelized_b: [Complex<f32>; 2048] = channelize(&pol_b_time_series, &fft);
+
+        // Convert to power and add to spectra
+        for i in 0..=spectra.len() {
+            spectra[i] = channelized_a[i].norm() + channelized_b[i].norm();
+        }
+
+        // Metrics
         cnt += PAYLOAD_SIZE;
         if last_reported.elapsed().as_secs_f32() >= 1.0 {
+            // Print perf
             last_reported = Instant::now();
             println!(
                 "Rate - {} Gb/s",
                 (cnt as f64) / program_start.elapsed().as_secs_f64() / 1.25e8
             );
+            let mut wtr = csv::Writer::from_writer(io::stdout());
+            wtr.write_record(spectra.map(|e| e.to_string()))?;
+            // Bail
+            process::exit(0);
         }
     }
 }
