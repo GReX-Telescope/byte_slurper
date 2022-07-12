65819c4191d49b57673f15fe20eddebd6def1b53
Kiran Shila <me@kiranshila.com>
2022-07-12 13:56:46 -0700

    Add work ratio

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ use std::time::Instant;
 fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind("192.168.5.1:60000")?;
     let mut buf = [0u8; 8192];
-    let mut cnt = 0u64;
+    let mut work_time = 0f64;
+    let mut idle_time = 0f64;
+
     let mut last_reported = Instant::now();
+    let mut b = Instant::now();
     loop {
+        let a = Instant::now();
+        work_time += a.duration_since(b).as_secs_f64();
         socket.recv(&mut buf)?;
-        cnt += 1;
+        b = Instant::now();
+        idle_time += b.duration_since(a).as_secs_f64();
         if last_reported.elapsed().as_secs_f32() >= 1.0 {
             last_reported = Instant::now();
-            println!("{} - Payloads", cnt);
+            println!("Work Ratio - {}", work_time / (work_time + idle_time));
         }
     }
 }
