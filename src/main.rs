d016fa0ccd5859e3c4b83245d0e1aadf4708fafa
Kiran Shila <me@kiranshila.com>
2022-07-21 20:05:39 -0700

    Simplify

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,12 @@ use std::default::Default;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::UdpSocket;
-use std::time::Instant;
 
 fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind("192.168.5.1:60000")?;
     let stokes_stream = TcpListener::bind("0.0.0.0:4242")?;
     let (mut stokes_socket, _) = stokes_stream.accept()?;
     let mut buf = [0u8; PAYLOAD_SIZE];
-    let mut cnt = 0usize;
-
-    let mut last_reported = Instant::now();
-    let program_start = Instant::now();
 
     let mut pol_x = [ComplexByte::default(); CHANNELS];
     let mut pol_y = [ComplexByte::default(); CHANNELS];
@@ -22,6 +17,8 @@ fn main() -> std::io::Result<()> {
     let mut stokes = [0f32; CHANNELS];
     let mut stokes_accum = [0f32; CHANNELS];
 
+    let mut sums = 0u64;
+    println!("Here we go! (Mario voice)");
     loop {
         // Grab incoming data
         let n = socket.recv(&mut buf)?;
@@ -31,19 +28,15 @@ fn main() -> std::io::Result<()> {
         payload_to_spectra(&buf, &mut pol_x, &mut pol_y);
         stokes_i(&pol_x, &pol_y, &mut stokes);
         // Sum stokes
-        vsum_mut(&stokes, &mut stokes_accum, 122070); // Packets per s
+        vsum_mut(&stokes, &mut stokes_accum, 30000); // Packets per s
 
         // Metrics
-        cnt += PAYLOAD_SIZE;
-        if last_reported.elapsed().as_secs_f32() >= 0.1 {
-            // Print perf
-            last_reported = Instant::now();
-            println!(
-                "Rate - {} Gb/s\t",
-                (cnt as f64) / program_start.elapsed().as_secs_f64() / 1.25e8,
-            );
+        sums += 1;
+
+        if sums == 30000 {
             stokes_socket.write_all(stokes_accum.as_byte_slice())?;
             stokes_accum = [0f32; CHANNELS];
+            sums = 0;
         }
     }
 }
