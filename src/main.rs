47e0f55607b12521cf7ef0fcf481f816b1802366
Kiran Shila <me@kiranshila.com>
2022-07-24 10:47:48 -0700

    Multithread

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,16 @@ use std::default::Default;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::UdpSocket;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::thread;
 use std::time::Instant;
 
 const AVG_SIZE: usize = 10000;
 
-fn main() -> std::io::Result<()> {
-    let socket = UdpSocket::bind("192.168.5.1:60000")?;
-    let stokes_stream = TcpListener::bind("0.0.0.0:4242")?;
-    let (mut stokes_socket, _) = stokes_stream.accept()?;
-    let mut buf = [0u8; PAYLOAD_SIZE];
-
-    let mut pol_x = [ComplexByte::default(); CHANNELS];
-    let mut pol_y = [ComplexByte::default(); CHANNELS];
+fn stokes_consumer(reciever: Receiver<([ComplexByte; 2048], [ComplexByte; 2048])>) {
+    let stokes_stream = TcpListener::bind("0.0.0.0:4242").unwrap();
+    let (mut stokes_socket, _) = stokes_stream.accept().unwrap();
 
     let mut stokes = [0f32; CHANNELS];
     let mut stokes_accum = [0f32; CHANNELS];
@@ -25,14 +23,8 @@ fn main() -> std::io::Result<()> {
 
     let mut last_reported = Instant::now();
 
-    println!("Here we go! (Mario voice)");
-    loop {
-        // Grab incoming data
-        let n = socket.recv(&mut buf)?;
-        if n != PAYLOAD_SIZE {
-            continue;
-        }
-        payload_to_spectra(&buf, &mut pol_x, &mut pol_y);
+    for (pol_x, pol_y) in reciever {
+        // Grab from channel
         stokes_i(&pol_x, &pol_y, &mut stokes);
         // Sum stokes
         vsum_mut(&stokes, &mut stokes_accum, AVG_SIZE as u32);
@@ -44,7 +36,9 @@ fn main() -> std::io::Result<()> {
         if sums == AVG_SIZE {
             let rate = (cnt as f32) / last_reported.elapsed().as_secs_f32() / 1.25e8;
             println!("TX Cycle! Rate - {} Gb/s", rate);
-            stokes_socket.write_all(stokes_accum.as_byte_slice())?;
+            stokes_socket
+                .write_all(stokes_accum.as_byte_slice())
+                .unwrap();
             // Resets
             stokes_accum = [0f32; CHANNELS];
             sums = 0;
@@ -52,4 +46,34 @@ fn main() -> std::io::Result<()> {
             last_reported = Instant::now();
         }
     }
+}
+
+fn main() -> std::io::Result<()> {
+    let socket = UdpSocket::bind("192.168.5.1:60000")?;
+
+    // Setup multithreading
+    let (sender, receiver) = mpsc::channel();
+
+    // Start producing polarizations on a thread
+    thread::spawn(move || {
+        let mut buf = [0u8; PAYLOAD_SIZE];
+        let mut pol_x = [ComplexByte::default(); CHANNELS];
+        let mut pol_y = [ComplexByte::default(); CHANNELS];
+        loop {
+            // Grab incoming data
+            let n = socket.recv(&mut buf).unwrap();
+            // Skip bad packets
+            if n != PAYLOAD_SIZE {
+                continue;
+            }
+            // Unpack
+            payload_to_spectra(&buf, &mut pol_x, &mut pol_y);
+            // Send to channel
+            sender.send((pol_x, pol_y)).unwrap();
+        }
+    });
+
+    // Start consumer
+    stokes_consumer(receiver);
+    Ok(())
 }
