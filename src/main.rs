eb53d79c615bc9a3b6e98c34c065af62167b8c12
Kiran Shila <me@kiranshila.com>
2022-07-27 17:45:26 -0700

    Produce averages from main thread

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,90 @@
 use byte_slice_cast::AsByteSlice;
 use byte_slurper::*;
 use chrono::Utc;
-use crossbeam_channel::{bounded, Receiver};
+use crossbeam_channel::{bounded, Receiver, Sender};
 use pnet::{
     packet::{ip::IpNextHeaderProtocols, Packet},
     transport::{
         transport_channel, udp_packet_iter, TransportChannelType::Layer4, TransportProtocol::Ipv4,
+        TransportReceiver,
     },
 };
 use psrdada::DadaDBBuilder;
 use std::{default::Default, thread};
 
-fn stokes_to_dada(
-    reciever: Receiver<([ComplexByte; CHANNELS], [ComplexByte; CHANNELS])>,
-    mut writer: psrdada::WriteHalf,
-) {
-    let mut avg_window = [0i16; AVG_WINDOW_SIZE];
+fn stokes_to_dada(receiver: Receiver<[i16; CHANNELS]>, mut writer: psrdada::WriteHalf) {
     let mut window = [0i16; WINDOW_SIZE];
-
-    let mut avg_cnt = 0usize;
     let mut stokes_cnt = 0usize;
-
     let mut first_sample_time = Utc::now();
 
-    for (pol_x, pol_y) in reciever {
-        let avg_slice = &mut avg_window[(avg_cnt * CHANNELS)..((avg_cnt + 1) * CHANNELS)];
-        // Grab from channel and push stokes to average
-        gen_stokes_i(&pol_x, &pol_y, avg_slice);
-        avg_cnt += 1;
-        if avg_cnt == (AVG_SIZE - 1) {
-            // If this is the first sample in the output window, mark the time
-            if stokes_cnt == 0 {
-                first_sample_time = Utc::now();
+    for stokes in receiver {
+        // Push the incoming average to the right place in the output
+        window[(stokes_cnt * CHANNELS)..((stokes_cnt + 1) * CHANNELS)].clone_from_slice(&stokes);
+        // If this was the first one, update the start time
+        if stokes_cnt == 0 {
+            first_sample_time = Utc::now();
+        }
+        // Increment the stokes counter
+        stokes_cnt += 1;
+        // If we've filled the window, generate the header and send the whole thing
+        if stokes_cnt == WINDOW_SIZE {
+            // Reset the stokes counter
+            stokes_cnt = 0;
+            // Most of these should be constants or set by args
+            let header = gen_header(
+                CHANNELS as u32,
+                250f32,
+                1405f32,
+                1,
+                16,
+                TSAMP,
+                &heimdall_timestamp(first_sample_time),
+            );
+            println!("Sending data to heimdall via PSRDADA");
+            writer.push_header(&header).unwrap();
+            writer.push(window.as_byte_slice()).unwrap();
+        }
+    }
+}
+
+fn udp_to_avg(mut udp_rx: TransportReceiver, port: u16, sender: Sender<[i16; CHANNELS]>) {
+    // Locals
+    let mut pol_x = [ComplexByte::default(); CHANNELS];
+    let mut pol_y = [ComplexByte::default(); CHANNELS];
+    // State to hold the averaging window
+    let mut avg_window = [0i16; AVG_WINDOW_SIZE];
+    let mut avg = [0i16; CHANNELS];
+    let mut avg_cnt = 0usize;
+    // Capture packets
+    let mut iter = udp_packet_iter(&mut udp_rx);
+    loop {
+        match iter.next() {
+            Ok((packet, _)) => {
+                // Skip invalid packets
+                if packet.get_destination() != port {
+                    continue;
+                }
+                if packet.get_length() as usize != PAYLOAD_SIZE {
+                    continue;
+                }
+                // Unpack
+                payload_to_spectra(packet.packet(), &mut pol_x, &mut pol_y);
+                // --- Average
+                // Generate stokes and push to averaging window
+                let avg_slice = &mut avg_window[(avg_cnt * CHANNELS)..((avg_cnt + 1) * CHANNELS)];
+                gen_stokes_i(&pol_x, &pol_y, avg_slice);
+                avg_cnt += 1;
+                if avg_cnt == CHANNELS {
+                    // Reset the counter
+                    avg_cnt = 0;
+                    // Generate average
+                    avg_from_window(&avg_window, &mut avg, CHANNELS);
+                    // Send to channel
+                    sender.send(avg).unwrap();
+                }
             }
-            // Average the averaging window, push to output window
-            let stokes_slice = &mut window[(stokes_cnt * CHANNELS)..((stokes_cnt + 1) * CHANNELS)];
-            avg_from_window(&avg_window, stokes_slice, CHANNELS);
-            // Increment the stokes counter
-            stokes_cnt += 1;
-            // Reset the averaging counter
-            avg_cnt = 0;
-            // If we've filled the window
-            // generate the header and send the whole thing
-            if stokes_cnt == (WINDOW_SIZE - 1) {
-                // Reset the counter
-                stokes_cnt = 0;
-                // Most of these should be constants or set by args
-                let header = gen_header(
-                    CHANNELS as u32,
-                    250f32,
-                    1405f32,
-                    1,
-                    16,
-                    TSAMP,
-                    &heimdall_timestamp(first_sample_time),
-                );
-                println!("Sending data to heimdall via PSRDADA");
-                writer.push_header(&header).unwrap();
-                writer.push(window.as_byte_slice()).unwrap();
+            Err(e) => {
+                eprintln!("Packet next error - {}", e);
             }
         }
     }
@@ -68,47 +95,15 @@ fn main() -> std::io::Result<()> {
     let port = 60000u16;
     let dada_key = 0xbeef;
 
-    // Deal with shutdowns properly
-    ctrlc::set_handler(move || {
-        println!("Bringing down!");
-        std::process::exit(0);
-    })
-    .expect("Error setting Ctrl-C handler");
-
     // We're going to be looking at Layer 4, IpV4, UDP data
-    let (_, mut udp_rx) =
-        transport_channel(PAYLOAD_SIZE * 2, Layer4(Ipv4(IpNextHeaderProtocols::Udp)))
-            .expect("Error creating transport channel");
+    let (_, udp_rx) = transport_channel(PAYLOAD_SIZE * 2, Layer4(Ipv4(IpNextHeaderProtocols::Udp)))
+        .expect("Error creating transport channel");
 
     // Setup multithreading
-    let (sender, receiver) = bounded(1000);
+    let (stokes_sender, stokes_receiver) = bounded(1000);
 
     // Start producing polarizations on a thread
-    thread::spawn(move || {
-        let mut pol_x = [ComplexByte::default(); CHANNELS];
-        let mut pol_y = [ComplexByte::default(); CHANNELS];
-        let mut iter = udp_packet_iter(&mut udp_rx);
-        loop {
-            match iter.next() {
-                Ok((packet, _)) => {
-                    // Skip invalid packets
-                    if packet.get_destination() != port {
-                        continue;
-                    }
-                    if packet.get_length() as usize != PAYLOAD_SIZE {
-                        continue;
-                    }
-                    // Unpack
-                    payload_to_spectra(packet.packet(), &mut pol_x, &mut pol_y);
-                    // Send to PSRDADA
-                    sender.send((pol_x, pol_y)).unwrap();
-                }
-                Err(e) => {
-                    eprintln!("Packet next error - {}", e);
-                }
-            }
-        }
-    });
+    thread::spawn(move || udp_to_avg(udp_rx, port, stokes_sender));
 
     // Setup PSRDADA
     let hdu = DadaDBBuilder::new(dada_key, "byte_slurper")
@@ -119,6 +114,6 @@ fn main() -> std::io::Result<()> {
     let (_, writer) = hdu.split();
 
     // Start consumer
-    stokes_to_dada(receiver, writer);
+    stokes_to_dada(stokes_receiver, writer);
     Ok(())
 }
