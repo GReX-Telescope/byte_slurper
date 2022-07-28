89276e5affe82badc69a8dbb79ddfcc5307b3f4f
Kiran Shila <me@kiranshila.com>
2022-07-27 18:15:21 -0700

    Bound channel even more

--- src/main.rs
+++ src/main.rs
@@ -102,7 +102,7 @@ fn main() -> std::io::Result<()> {
         .expect("Error creating transport channel");
 
     // Setup multithreading
-    let (stokes_sender, stokes_receiver) = bounded(1000);
+    let (stokes_sender, stokes_receiver) = bounded(1);
 
     // Start producing polarizations on a thread
     thread::spawn(move || udp_to_avg(udp_rx, port, stokes_sender));
