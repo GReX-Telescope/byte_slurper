d5ee64797592282da01d06ba2ad306519c48d1c0
Kiran Shila <me@kiranshila.com>
2022-08-02 16:05:42 -0700

    Big average

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub const PAYLOAD_SIZE: usize = 8192;
 pub const WORD_SIZE: usize = 8;
 pub const CHANNELS: usize = 2048;
 
-pub const AVG_SIZE: usize = 4; // At tsamp of 8.192 us, this gives us 1 stoke per 65.536us
+pub const AVG_SIZE: usize = 16; // At tsamp of 8.192 us, this gives us 1 stoke per 65.536us
 pub const NSAMP: usize = 16384; // At stoke time of 65.536, this is a little more than a second
 pub const WINDOW_SIZE: usize = CHANNELS * NSAMP;
 // A buffer for the running average
