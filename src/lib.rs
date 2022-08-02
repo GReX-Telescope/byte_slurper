e092a46b079c2ef84b0ed81b1e70dc31bdd4ecfa
Kiran Shila <me@kiranshila.com>
2022-08-02 14:06:58 -0700

    Fixed point math

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+use az::Cast;
 use chrono::{DateTime, Datelike, Timelike, Utc};
-use nalgebra::{ArrayStorage, Const, Matrix, OMatrix, SMatrix};
-use num_complex::Complex;
+use fixed::{
+    types::extra::{U14, U7},
+    FixedI16, FixedI8,
+};
 
 pub const PAYLOAD_SIZE: usize = 8192;
 pub const WORD_SIZE: usize = 8;
@@ -14,30 +17,34 @@ pub const AVG_WINDOW_SIZE: usize = AVG_SIZE * CHANNELS;
 // We can figure out sample time
 pub const TSAMP: f32 = 8.192e-6 * AVG_SIZE as f32;
 
-pub type ComplexByte = Complex<i8>;
+#[derive(Clone, Copy, Default)]
+pub struct Complex<T> {
+    re: T,
+    im: T,
+}
 
-// Upcast to avoid overflow
-fn square_byte(byte: i8) -> u16 {
-    (byte as i16 * byte as i16) as u16
+pub type FpgaByte = FixedI8<U7>;
+pub type FixedWord = FixedI16<U14>;
+pub type ComplexByte = Complex<FpgaByte>;
+
+fn square_byte(byte: FpgaByte) -> FixedWord {
+    byte.wide_mul(byte)
 }
 
 // If we need to, these can be unchecked-add
-fn norm_sq(cb: ComplexByte) -> u16 {
+fn norm_sq(cb: ComplexByte) -> FixedWord {
     square_byte(cb.re) + square_byte(cb.im)
 }
 
-pub fn stokes_i(pol_x: ComplexByte, pol_b: ComplexByte) -> u16 {
-    norm_sq(pol_x) + norm_sq(pol_b)
+// We're done multiplying, so we can come back to u16 land
+pub fn stokes_i(pol_x: ComplexByte, pol_y: ComplexByte) -> u16 {
+    let pol_x: u16 = norm_sq(pol_x).cast();
+    let pol_y: u16 = norm_sq(pol_y).cast();
+    pol_x + pol_y
 }
 
-pub fn gen_stokes_i<const N: usize>(
-    pol_x: &[ComplexByte; N],
-    pol_y: &[ComplexByte; N],
-    output: &mut [u16],
-) {
-    for i in 0..N {
-        output[i] = stokes_i(pol_x[i], pol_y[i]);
-    }
+fn raw_to_fpga(byte: u8) -> FpgaByte {
+    FpgaByte::from_bits(byte as i8)
 }
 
 pub fn payload_to_spectra(
@@ -48,22 +55,22 @@ pub fn payload_to_spectra(
     for (i, word) in payload.chunks_exact(WORD_SIZE).enumerate() {
         // Each word contains two frequencies for each polarization
         // [A1 B1 A2 B2]
-        // Where each channel is [Re Im]
+        // Where each channel is [Re Im] as FixedI8<7>
         let a1 = ComplexByte {
-            re: word[7] as i8,
-            im: word[6] as i8,
+            re: raw_to_fpga(word[7]),
+            im: raw_to_fpga(word[6]),
         };
         let a2 = ComplexByte {
-            re: word[5] as i8,
-            im: word[4] as i8,
+            re: raw_to_fpga(word[5]),
+            im: raw_to_fpga(word[4]),
         };
         let b1 = ComplexByte {
-            re: word[3] as i8,
-            im: word[2] as i8,
+            re: raw_to_fpga(word[3]),
+            im: raw_to_fpga(word[2]),
         };
         let b2 = ComplexByte {
-            re: word[1] as i8,
-            im: word[0] as i8,
+            re: raw_to_fpga(word[1]),
+            im: raw_to_fpga(word[0]),
         };
         // Update spectra
         pol_a[2 * i] = a1;
