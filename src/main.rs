ecacf78401973f6a711e546befb5ffbc5236a8ee
Kiran Shila <me@kiranshila.com>
2022-08-02 15:47:14 -0700

    No more prints

--- src/main.rs
+++ src/main.rs
@@ -122,7 +122,6 @@ fn udp_to_avg(
             // Generate average
             let avg = &mut *avg_mutex.lock().unwrap();
             avg_from_window::<CHANNELS>(&avg_window, avg);
-            println!("{:?}", avg);
             // Signal the consumer that there's new data
             sig_tx.send(Signal::NewAvg).unwrap();
         }
